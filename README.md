3918833776251b97787652a77f5c9022e17c2538
KazamiMichiru <matthewdm0816@gmail.com>
2021-03-05 17:25:49 +0800

    added baseline bf

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 ### AMAFILTER
+Project: Bilateral Graph Filter for Graph Signal Denoising
---
+++ bf-baseline.py
@@ -0,0 +1,10 @@
+"""
+Bilateral Filter Baseline: Manual Adjusted Filters
+TODO: 
+    1. Test on modelnet40
+"""
+
+import torch
+
+class NaiveBilateralFilter(torch.Module)
+
