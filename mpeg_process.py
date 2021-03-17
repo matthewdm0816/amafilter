1011eb34ec8bc3400eed3747acf95a7dc8f83f99
KazamiMichiru <matthewdm0816@gmail.com>
2021-03-18 01:42:31 +0800

    updated mpeg process

--- mpeg_process.py
+++ mpeg_process.py
@@ -28,37 +28,44 @@ out_paths = ["longdress.pt"]
 
 
 def read_mesh(path):
-    print(colorama.Fore.MAGENTA + "Loading PLY file %s" % path)
+    r"""
+    Read mesh XYZ/RGB from PLY file
+    """
+    print(colorama.Fore.RED + "Loading PLY file %s" % path)
     mesh = PlyData.read(path)
     x, y, z = (
-        torch.from_numpy(mesh.elements[0].data["x"]),
-        torch.from_numpy(mesh.elements[0].data["y"]),
-        torch.from_numpy(mesh.elements[0].data["z"]),
+        torch.from_numpy(mesh.elements[0].data["x"].copy()),
+        torch.from_numpy(mesh.elements[0].data["y"].copy()),
+        torch.from_numpy(mesh.elements[0].data["z"].copy()),
     )
     r, g, b = (
-        torch.from_numpy(mesh.elements[0].data["red"]),
-        torch.from_numpy(mesh.elements[0].data["green"]),
-        torch.from_numpy(mesh.elements[0].data["blue"]),
+        torch.from_numpy(mesh.elements[0].data["red"].copy()),
+        torch.from_numpy(mesh.elements[0].data["green"].copy()),
+        torch.from_numpy(mesh.elements[0].data["blue"].copy()),
     )
     pos = torch.cat([x, y, z], dim=-1).to(torch.float32)
     color = torch.cat([r, g, b], dim=-1).to(torch.float32)
     # Asserting both [N, 3] shape
     assert pos.shape == color.shape
-    return Data(x=color, pos=pos)
+    return Data(color=color, pos=pos)
 
 
-def process_ply(ply_path: str, n_patch: int = 100):
+def process_ply(ply_path: str, n_patch: int = 100, k: int=2048):
     r"""
     Processes PLY file from path, convert to PC
     :return patch-pos + patch-color + patch-kernel
     """
-    mesh_data = read_ply(ply_path)
-    pos, color = mesh_data.pos, mesh_data.x
+    mesh_data = read_mesh(ply_path)
+    print(colorama.Fore.GREEN + "Loaded PLY file %s" % ply_path)
+    pos, color = mesh_data.pos, mesh_data.color
     n_pts = pos.shape[0]
     # select N kernels by FPS sample: [N, ]
-    patch_kernel = fps(pos, ratio=n_patch / n_pts + 1e-6)
+    patch_kernel = fps(pos, ratio=n_patch / n_pts)
+    sprint(patch_kernel.shape)
     # select N patches: [N, 2048]
-    patches = knn(pos[patch_kernel], pos, k=2048, num_workers=16)
+    patches = knn(pos, pos[patch_kernel], k=k, num_workers=32)
+    patches = patches[1].reshape(n_patch, k)
+    sprint(patches.shape)
     data_list = [
         # ([N, 2048, 3], [N, 2048, 3], [N, 3])
         Data(color=color[patch], pos=pos[patch], kernel_pos=pos[pk])
